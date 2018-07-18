5e95d48577569b7945b313f19b1588d4f3bab072
Qhub <qdaman90@gmail.com>
2018-07-17 19:18:11 -0500

    version1

--- assets/JavaScript/logic.js
+++ assets/JavaScript/logic.js
@@ -10,17 +10,17 @@ var config = {
 firebase.initializeApp(config);
 
 // A function that does an AJAX call
-function judgesInfo(judge) {
+function judgesInfo(firstName, lastName) {
 $.ajax({
-    url: "https://datacatalog.cookcountyil.gov/resource/69z9-nyig.json?position=Judge&last_name=" + judge,
+    url: "https://datacatalog.cookcountyil.gov/resource/69z9-nyig.json?position=Judge&first_name=" + firstName +"&last_name=" + lastName,
     type: "GET",
     data: {
         "$limit" : 5000,
         "$$app_token" : "z19GcEXHUsdQi8X7j8doySfMi"
     }
 }).done(function(data) {
-
-   var fullName =data[0].first_name + " " + data[0].last_name;
+  
+    var fullName =data[0].first_name + " " + data[0].last_name;
 
     //  Dynamically creating a new row for the table
    var tableRow = $("<tr>");
@@ -36,15 +36,21 @@ $.ajax({
       tableRow.append(judgeLocation);
 
    // Finding our table element and appending the row we just made
-     $("#employeeTable").append(tableRow);
+     $("#judgeTable").append(tableRow);
  });
 }
-//   on Click that grabs the text from the Input box and calls our judgesInfo() function
-$("#select-artist").on("click", function(event) {
+//   on Click that grabs the text from the Input box and calls our judgesInfo() function also capitalizes the first letter of each input
+$("#submit").on("click", function(event) {
 
     event.preventDefault();
-    var lastName = $("#artist-input").val().trim();
-    console.log(lastName);
+    
+    var firstName = $("#firstName").val().trim();
+     firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
 
-    judgesInfo(lastName);
+    var lastName = $("#lastName").val().trim();
+      lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+    
+    
+
+    judgesInfo(firstName, lastName);
 })
